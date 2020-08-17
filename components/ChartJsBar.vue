4b335ece01d3e94dc27bee037c311329f90f3b53
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-08-18 08:13:14 +0900

    vue-chart.jsの実装

---
+++ components/ChartJsBar.vue
@@ -0,0 +1,12 @@
+<script>
+import { Bar } from 'vue-chartjs'
+export default {
+  extends: Bar,
+  props:['chartdata', 'options'],
+  methods: {
+    init() {
+      this.renderChart(this.chartdata, this.options);
+    }
+  },
+}
+</script>
